cd5e47e27883d93228546ee66bceb1480ee9fa19
Gasman <90539473+MarybethGasman@users.noreply.github.com>
2023-08-06 17:32:36 +0800

    Update flutter.rb

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -5,14 +5,14 @@ cask "flutter" do
   sha256 arm:   "02ec7d7805b45e65bbc42b6b48bd22523e58d9d2fc36ab443c12ba8535802158",
          intel: "64e6b722e9ebdf9ccc83ef9253f24a3c0519d103e622aeb0c0e7c7647636f1a5"
 
-  url "https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos#{arch}_#{version}-stable.zip",
-      verified: "storage.googleapis.com/flutter_infra_release/releases/stable/macos/"
+  url "https://storage.flutter-io.cn/flutter_infra_release/releases/stable/macos/flutter_macos#{arch}_#{version}-stable.zip",
+      verified: "storage.flutter-io.cn/flutter_infra_release/releases/stable/macos/"
   name "Flutter SDK"
   desc "UI toolkit for building applications for mobile, web and desktop"
   homepage "https://flutter.dev/"
 
   livecheck do
-    url "https://storage.googleapis.com/flutter_infra_release/releases/releases_macos.json"
+    url "https://storage.flutter-io.cn/flutter_infra_release/releases/releases_macos.json"
     regex(%r{/flutter[._-]macos[._-]v?(\d+(?:\.\d+)+)[._-]stable\.zip}i)
   end
 
